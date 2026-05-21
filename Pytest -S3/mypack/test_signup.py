2f0c946ee4a5b861a4b0d607bec31c1b2b004bf4
PurvaBorade22 <boradepurva1@gmail.com>
2026-05-21 21:34:26 +0530

    login and signup example with common fixture (conft file)

---
+++ Pytest -S3/mypack/test_signup.py
@@ -0,0 +1,14 @@
+import pytest
+
+def signupby_email(setup):
+    print("Signup by email")
+    assert True == True  #: Assertion is nothing but the validation point.
+
+def signupby_facebook(setup):
+    print("Signup by facebook")
+    assert True == True     
+    
+def signupby_google(setup):      
+    print("Signup by google")
+    assert True == True
+    
