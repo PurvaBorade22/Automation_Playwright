b81ef1ae6f3b1f388a58c0fb01deeb70f5f652e6
PurvaBorade22 <boradepurva1@gmail.com>
2026-05-22 16:49:42 +0530

    Parallel testing

---
+++ Section 3-Pytest/Day-2_features/test_parallel.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_one():
+    print("Running test one")
+
+def test_two():
+    print("Running test two")   
+    
+def test_three():  
+    print("Running test three")
+    
+def test_four():
+    print("Running test four")
