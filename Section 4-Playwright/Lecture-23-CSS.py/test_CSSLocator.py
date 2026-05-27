7e1c0d45f663ca35fff950a25b188fa2e80f97e2
PurvaBorade22 <boradepurva1@gmail.com>
2026-05-27 18:42:10 +0530

    Css locator - tag.class

--- Section 4-Playwright/Lecture-23-CSS.py/test_CSSLocator.py
+++ Section 4-Playwright/Lecture-23-CSS.py/test_CSSLocator.py
@@ -1,9 +1,18 @@
+
+# tag id          tag#id
+# tag class       tag.class
+# tag attribute   tag[attribute=value]
+# tag class attribute     tag.class[attribute=value]
+
 from playwright.sync_api import Page, expect
 
 def test_verify_css_locator(page:Page):
     page.goto("https://demowebshop.tricentis.com/")
     
-    # 1) tag.id - tag#id
-    page.locator("#small-searchterms").fill("T-shirt")
-    page.wait_for_timeout(5000)
+    # # 1) tag id - tag#id
+    # page.locator("#small-searchterms").fill("T-shirt")
+    # page.wait_for_timeout(5000)
     
+    # 2) tag class - tag.class
+    page.locator("input.search-box-text").fill("T-shirt")
+    page.wait_for_timeout(5000)  
