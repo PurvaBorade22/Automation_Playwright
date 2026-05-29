87590254c910eebff2c4a72f5cfeaf9fd126ed75
PurvaBorade22 <boradepurva1@gmail.com>
2026-05-29 17:54:24 +0530

    Contain methods in xpath

--- Section 4-Playwright/Lecture-24-XPath Locators/test_XPathLocators.py
+++ Section 4-Playwright/Lecture-24-XPath Locators/test_XPathLocators.py
@@ -12,3 +12,22 @@ def test_xpath_locators(page:Page):
     logo = page.locator("//img[@alt='Tricentis Demo Web Shop']")
     expect(logo).to_be_visible()
     page.wait_for_timeout(5000)
+    
+    # 3) xpath with contain
+    products= page.locator("//h2//a[contains(@href,'computer')]")
+    products_count= products.count()
+    print("Products count:",products_count)
+    expect(products).to_have_count(products_count)
+    
+    # 4) product first,last, and nth name
+    print("Product first name is:",products.first.text_content())
+    print("Products last name is:",products.last.text_content())
+    print("Product nth name is:",products.nth(3).text_content())
+    
+    product_titles = products.all_text_contents()
+    print("All the products contain are:", product_titles)
+    
+    for i in product_titles:
+        print(i)
+        
+    
